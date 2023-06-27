a754f32c62924cc09075ae8483d4fe4c84e3f61d
Tamires <79336444+tamicristina@users.noreply.github.com>
2023-06-26 21:16:39 -0300

    refact: changing component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,8 @@ import { MatInputModule } from "@angular/material/input";
 import { MatFormFieldModule } from "@angular/material/form-field";
 import { FormsModule } from "@angular/forms";
 import { MatIconModule } from "@angular/material/icon";
+import { MatDividerModule } from "@angular/material/divider";
+import { MatButtonModule } from "@angular/material/button";
 
 @NgModule({
   declarations: [AppComponent, HomeComponent, SearchInputComponent],
@@ -21,6 +23,8 @@ import { MatIconModule } from "@angular/material/icon";
     MatFormFieldModule,
     FormsModule,
     MatIconModule,
+    MatDividerModule,
+    MatButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/components/search-input/search-input.component.ts
+++ src/app/components/search-input/search-input.component.ts
@@ -1,8 +1,9 @@
-import { Component } from "@angular/core";
+import { Component, ViewEncapsulation } from "@angular/core";
 
 @Component({
   selector: "app-search-input",
   templateUrl: "./search-input.component.html",
   styleUrls: ["./search-input.component.css"],
+  // encapsulation: ViewEncapsulation.None,
 })
 export class SearchInputComponent {}
